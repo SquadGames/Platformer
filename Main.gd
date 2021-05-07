a36f3405fbdac8f810eadb3e06490b6a7b0bfea5
Jesse B. Miller <jesse@jessebmiller.com>
2021-05-07 09:13:02 -0500

    quit, local levels

--- Main.gd
+++ Main.gd
@@ -1,28 +1,52 @@
 extends Node2D
 
 const LEVEL_ADDRESS_TEMPLATE = "https://raw.githubusercontent.com/SquadGames/Platformer/main/levels/{level_name}.tscn"
+const LOCAL_LEVEL_TEMPLATE = "res://levels/{level_name}.tscn"
 const WORLD_ADDRESS_TEMPLATE = "https://raw.githubusercontent.com/SquadGames/Platformer/main/worlds/{world_name}.json"
 var starting_player_position = Vector2(0, 0)
 var selected_level = "user://selectedLevel.tscn"
+var entered_level = ""
+
 var loaded_level
 var paused = true
 
 func _ready():
 	$Player.visible = false
-	get_and_load_world("default_world")
 	pause_game()
+	$GUI/VBoxContainer/HBoxContainer/LocalLevelName.connect(
+		"text_changed", self, "set_entered_level"
+	)
+	$GUI/VBoxContainer/HBoxContainer/LocalLevelButton.connect(
+		"button_up", self, "load_local_level"
+	)
+	$GUI/VBoxContainer/QuitButton.connect("button_up", self, "quit")
+	get_and_load_world("default_world")
+
+
+func quit():
+	get_tree().quit()
+
+
+func set_entered_level(text):
+	entered_level = text
+	print("entered ", entered_level)
 
 
 func get_and_load_world(world_name):
 	$getSelectedWorld.request(WORLD_ADDRESS_TEMPLATE.format({"world_name": world_name}))
 
 
-func get_and_load_level(level_name):
+func get_and_load_level(level_name, template = LEVEL_ADDRESS_TEMPLATE):
+	selected_level = "user://selectedLevel.tscn"
 	var dir = Directory.new()
 	dir.remove(selected_level)
 	$Player.visible = false
-	$getSelectedLevel.request(LEVEL_ADDRESS_TEMPLATE.format({"level_name": level_name}))
+	$getSelectedLevel.request(template.format({"level_name": level_name}))
+	
 
+func load_local_level():
+	selected_level = LOCAL_LEVEL_TEMPLATE.format({"level_name": entered_level})
+	start_level()
 
 func _on_getSelectedWorld_request_completed(result, response_code, headers, body):
 	var text = body.get_string_from_utf8()
@@ -42,6 +66,7 @@ func _on_getSelectedLevel_request_completed(result, response_code, headers, body
 func start_level():
 	# load the selected level
 	remove_child(loaded_level)
+	print("loading ", selected_level)
 	loaded_level = load(selected_level).instance()
 	add_child(loaded_level)
 	$Player.position = starting_player_position
