c0b3f286b295d3dc80c64f516d93bdb403a72b47
Jesse B. Miller <jesse@jessebmiller.com>
2021-05-26 11:18:05 -0500

    fixes pausing bugs

--- PausedOverlay.gd
+++ PausedOverlay.gd
@@ -11,7 +11,7 @@ func _ready():
 
 
 func _unhandled_input(event):
-	if not event.is_action_pressed("start"):
+	if not event.is_action_pressed("start") or Singleton.game_state != Singleton.PLAYING:
 		return
 	raise()
 	visible = not visible
