b61cc0c3d62fc83fd60c1c1a8e1620e394586edd
Jesse B. Miller <jesse@jessebmiller.com>
2021-06-10 15:29:48 -0500

    makes what is this content remote

--- WhatIsThis.gd
+++ WhatIsThis.gd
@@ -4,7 +4,13 @@ extends Control
 func _ready():
 	pause_mode = Node.PAUSE_MODE_PROCESS
 	$BackButton.connect("pressed", self, "back", [$BackButton.destination])
+	$GetContent.request("https://raw.githubusercontent.com/SquadGames/Platformer/main/docs/what_is_this.md")
 
 
 func back(dest):
 	get_tree().change_scene(dest)
+
+
+func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+	var text = body.get_string_from_utf8()
+	$Content.text = text
